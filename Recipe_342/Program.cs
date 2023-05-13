35ca509117ba7b5945b88aee8620769d4f83570d
hayashin28 <hayashin.28@gmail.com>
2023-05-13 23:15:01 +0900

    c#_first_commit

---
+++ Recipe_342/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+var xls = MyExcelBook.Create("example.xlsx");
+xls.CreateSheet("MySheet");
+xls.Save();
+
+public sealed class MyExcelBook
+{
+    private XSSFWorkbook? _xssFWorkbook;
+    private ISheet? _sheet;
+
+    private MyExcelBook()
+    {
+        _xssFWorkbook = new XSSFWorkbook();
+    }
+
+    private string _filepath;
+    public static MyExcelBook Create(string filepath)
+    {
+        var obj = new MyExcelBook();
+        obj._filepath = filepath;
+        obj._xssFWorkbook = new XSSFWorkbook();
+        return obj; 
+    }
+
+    public void CreateSheet(string name) => _sheet = _xssFWorkbook.CreateSheet(name);
+
+    public void Save()
+    {
+        using var stream = new FileStream(_filepath, FileMode.Create);
+        _xssFWorkbook.Write(stream);
+    }
+}
