2ad26913ec6186f77fd92c6c7230a4b3bcae3aa2
hayashin28 <hayashin.28@gmail.com>
2023-05-22 21:56:40 +0900

    ClosedXML を使った実装

--- Recipe_342/Program.cs
+++ Recipe_342/Program.cs
@@ -1,6 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
 using Recipe_342;
 
 var xls = MyExcelBook.Create("example.xlsx");
-xls.CreateSheet("MySheet");
+xls.CreateSheet("mySheet");
 xls.Save();
+*/
+using ClosedXML.Excel;
+
+var path = "example.xlsx";
+// ブックを生成
+using (var workbook = new XLWorkbook())
+{
+    // シートを生成
+    var worksheet = workbook.Worksheets.Add("mySheet");
+    // 保存
+    workbook.SaveAs(path);
+}
+
+
+
+
