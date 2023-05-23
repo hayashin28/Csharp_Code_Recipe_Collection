e3bff23c3e856162c88772c3b63afaf13e50f935
hayashin28 <hayashin.28@gmail.com>
2023-05-23 19:24:07 +0900

    Recipe_344 修正

--- Recipe_344/Program.cs
+++ Recipe_344/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
 using Recipe_344;
 
 var xls = MyExcelBook.Open("example.xlsx");
@@ -8,3 +9,17 @@ for (int i = 0; i < 5; i++)
 	var val = xls.GetValue(2, i);
 	Console.WriteLine(val.ToString());
 }
+*/
+using ClosedXML.Excel;
+
+var path = "example.xlsx";
+
+// ブックを読み込む
+var workbook = new XLWorkbook(path);
+// 1シート目を取得
+var worksheet = workbook.Worksheets.Worksheet(1);
+for (int i = 1; i <= 5; i++)
+{
+	var val = worksheet.Cell(2, i).Value;
+	Console.WriteLine(val.ToString());
+}	
