46171684369b05c54e823a9f49da468b4f2c7165
hayashin28 <hayashin.28@gmail.com>
2023-05-28 17:45:16 +0900

    Recipe_342 ファイルをキーボード入力に改修

--- Recipe_342/Program.cs
+++ Recipe_342/Program.cs
@@ -6,18 +6,27 @@ var xls = MyExcelBook.Create("example.xlsx");
 xls.CreateSheet("mySheet");
 xls.Save();
 */
+using System;
+using System.IO;
 using ClosedXML.Excel;
 
-var path = "example.xlsx";
-// ブックを生成
-using (var workbook = new XLWorkbook())
+Console.WriteLine("ファイル名を入力して下さい。");
+try
 {
-    // シートを生成
-    var worksheet = workbook.Worksheets.Add("mySheet");
-    // 保存
-    workbook.SaveAs(path);
+    // キーボード入力を受け取る
+    string path = Console.ReadLine();
+    // ブックを生成
+    using (var workbook = new XLWorkbook())
+    {
+        // シートを生成
+        var worksheet = workbook.Worksheets.Add("mySheet");
+        Console.WriteLine(path);
+        // 拡張子を付けて保存
+        workbook.SaveAs(string.Concat(path, ".xlsx"));
+    }
+} 
+catch(Exception e)
+{
+    // エラー内容を出力
+	Console.WriteLine(e.Message);
 }
-
-
-
-
