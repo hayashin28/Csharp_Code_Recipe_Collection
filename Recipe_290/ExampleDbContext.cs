6831ef42a9c9037eea84abf171b7b2add1a13a3a
hayashin28 <hayashin.28@gmail.com>
2023-05-29 20:36:51 +0900

    Recipe_290 修正

--- Recipe_290/ExampleDbContext.cs
+++ Recipe_290/ExampleDbContext.cs
@@ -15,7 +15,7 @@ namespace DBSample.EF {
 */
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-                        optionsBuilder.UseNpgsql("Host=175.41.203.140; Port=5432; Username=hayashi; Password=ya53mmfe; Database=postgres01");
+            optionsBuilder.UseNpgsql("Host=175.41.203.140; Port=5432; Username=hayashi; Password=ya53mmfe; Database=postgres01");
         }
 
         public DbSet<ShohinModel> shohinModel { get; set; }
