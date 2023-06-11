aef7682a71dc6d5bb6eb1ace1f3df7a3040f2d00
hayashin28 <hayashin.28@gmail.com>
2023-06-11 21:25:43 +0900

    Recipe_290 微修正

--- Recipe_290/Program.cs
+++ Recipe_290/Program.cs
@@ -12,7 +12,8 @@ namespace DBSample
 
             foreach(var ShohinModel in contexts.shohinModel)
             {
-                Console.WriteLine("shohin_id:{0}, shohin_mei:{1}, shohin_bunrui:{2}, hanbai_tanka:{3}, shiire_tanka:{4}, torokubi:{5}", ShohinModel.Shohin_Id, ShohinModel.Shohin_Mei, ShohinModel.Shohin_Bunrui, ShohinModel.Hanbai_Tanka, ShohinModel.Shiire_Tanka, ShohinModel.Torokubi);
+                Console.WriteLine("shohin_id:{0}, shohin_mei:{1}, shohin_bunrui:{2}, hanbai_tanka:{3}, shiire_tanka:{4}, torokubi:{5}", 
+                                        ShohinModel.Shohin_Id, ShohinModel.Shohin_Mei, ShohinModel.Shohin_Bunrui, ShohinModel.Hanbai_Tanka, ShohinModel.Shiire_Tanka, ShohinModel.Torokubi);
             }
         }
     }
